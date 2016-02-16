091a4d90418db08a3c4b1c3d4818ab9aaf441abe
Pawel Psztyc <jarrodek@gmail.com>
2016-02-16 12:58:47 +0000

    Upgraded to Polymer 1.2

--- chrome-window.js
+++ chrome-window.js
@@ -1,4 +1,6 @@
-Polymer('chrome-window', {
+'use strict';
+Polymer({
+  is: 'chrome-window',
   /**
    * Fired when a new window has been created but before load event is called
    * in created window.
@@ -8,20 +10,17 @@ Polymer('chrome-window', {
    *
    * @event created
    */
-  /**
-   *
-   *
-   */
-  publish: {
+  properties: {
     /**
      * The URL for new window.
      * New window will be created each time the url atributte is changed
      *
-     * @attribute url
      * @type String
-     * @default ''
      */
-    'url': '',
+    url: {
+      type: String,
+      observer: '_urlChanged'
+    },
 
     /**
      * Id to identify the window. This will be used to remember the size and
@@ -30,11 +29,9 @@ Polymer('chrome-window', {
      * while another window with the same id already exists,
      * the currently opened window will be focused instead of creating a new window.
      *
-     * @attribute name
      * @type String
-     * @defailt undefined
      */
-    'name': undefined,
+    name: String,
     /**
      * Frame type: none or chrome (defaults to chrome).
      * For none, the -webkit-app-region CSS property can be used to apply
@@ -45,112 +42,110 @@ Polymer('chrome-window', {
      * You can also pass an object of FrameOptions which is available since Chrome 35.
      * See more at https://developer.chrome.com/apps/app_window#type-FrameOptions
      *
-     * @attribute frame
      * @type String|Object
-     * @defailt undefined
      */
-    'frame': undefined,
+    frame: Object,
     /**
      * The initial state of the window, allowing it to be created already
      * fullscreen, maximized, or minimized. Defaults to 'normal'.
      *
      * Possible values are: "normal", "fullscreen", "maximized", or "minimized"
      *
-     * @attribute state
      * @type String
-     * @defailt 'normal'
      */
-    'state': 'normal',
+    state: {
+      type: String,
+      value: 'normal'
+    },
     /**
      * If true, the window will be created in a windowhidden state.
      * Call show() on the window to show it once it has been created.
      * Defaults to false.
      *
-     * @attribute windowhidden
      * @type boolean
-     * @defailt false
      */
-    'windowhidden': false,
+    windowHidden: {
+      type: Boolean,
+      value: false,
+      observer: '_windowHiddenChanged'
+    },
     /**
      * If true the window is maximized.
      * You can also call maximize().
      *
-     * @attribute maximized
      * @type boolean
-     * @defailt false
      */
-    'maximized': false,
+    maximized: {
+      type: Boolean,
+      value: false,
+      observer: '_maximizedChanged'
+    },
     /**
      * If true the window is minimized.
      * You can also call minimize().
      *
-     * @attribute minimized
      * @type boolean
-     * @defailt false
      */
-    'minimized': false
+    minimized: {
+      type: Boolean,
+      value: false,
+      observer: '_minimizedChanged'
+    },
+    /**
+     * Created window object.
+     * For more details see https://developer.chrome.com/apps/app_window#type-AppWindow
+     *
+     * @type {Object}
+     */
+    window: {
+      type: Object,
+      readOnly: true
+    }
   },
 
   /**
-   * Created window object.
-   * For more details see https://developer.chrome.com/apps/app_window#type-AppWindow
+   * Create a new window when URL has changed.
    */
-  window: undefined,
-
-
-   /**
-    * Create a new window when URL has changed.
-    */
-  urlChanged: function(){
-     //TODO: create a new window
-     if(this.url && this.url.trim() !== ''){
-       this.create();
-     }
+  _urlChanged: function() {
+    //TODO: create a new window
+    if (this.url && this.url.trim() !== '') {
+      this.create();
+    }
   },
 
-  windowhiddenChanged: function(){
-     if(!this.window){
-       this.fire('error',{'message':'No active window available'});
-       return;
-     }
+  _windowHiddenChanged: function() {
+    if (!this.window) {
+      this.fire('error', {
+        'message': 'No active window available'
+      });
+      return;
+    }
 
-     var state = this.windowhidden;
-     if(!state || state === ''){
-       state = false;
-     }
-     if(typeof state === 'string'){
-       if(state === 'true'){
-         state = true;
-       } else {
-         state = false;
-       }
-     }
-
-     if(state){
-       this.window.hide();
-     } else {
-       this.window.show();
-     }
+    if (this.windowHidden) {
+      this.window.hide();
+    } else {
+      this.window.show();
+    }
   },
 
-  maximizedChanged: function(){
-    if(!this.window){
+  _maximizedChanged: function() {
+    if (!this.window) {
       this._reportMissingWindow();
       return;
     }
-    if(this.maximized){
+    if (this.maximized) {
       this.maximize();
     } else {
       this.restore();
     }
   },
 
-  minimizedChanged: function(){
-    if(!this.window){
+  _minimizedChanged: function() {
+    if (!this.window) {
       this._reportMissingWindow();
       return;
     }
-    if(this.minimized){
+    if (this.minimized) {
       this.minimize();
     } else {
       this.restore();
@@ -172,65 +167,61 @@ Polymer('chrome-window', {
    * other than the remembered default, you can create it hidden,
    * move it to the desired location, then show it.
    *
-   * @param options {Object} a create window options. See https://developer.chrome.com/apps/app_window#type-CreateWindowOptions
-   *                         for more details. If this object is not passed to the function
-   *                         default values from element's atributtes will be used.
+   * @param options {Object} a create window options.
+   * See https://developer.chrome.com/apps/app_window#type-CreateWindowOptions
+   * for more details. If this object is not passed to the function default values from
+   * element's atributtes will be used.
    */
-  create: function(options){
-    if(!options){
+  create: function(options) {
+    if (!options) {
       options = {};
     }
-    if(this.name){
+    if (this.name) {
       options.id = options.id || this.name;
     }
-    if(this.frame){
+    if (this.frame) {
       options.frame = options.frame || this.frame;
     }
-    if(this.state){
+    if (this.state) {
       options.state = options.state || this.state;
     }
 
-    var _hidden = this.windowhidden;
-    if(typeof _hidden === 'string'){
-      if(_hidden === 'true'){
-        _hidden = true;
-      } else {
-        _hidden = false;
-      }
-    }
-    options.hidden = _hidden;
+    options.hidden = this.windowHidden;
 
     var context = this;
-    chrome.app.window.create(this.url, options, function(createdWindow){
-      context.window = createdWindow;
-      context.fire('created', {createdWindow: createdWindow});
+    chrome.app.window.create(this.url, options, function(createdWindow) {
+      context._setWindow(createdWindow);
+      context.fire('created', {
+        createdWindow: createdWindow
+      });
     });
   },
 
-   /**
-    * Query for opened by the app windows.
-    * If `name` atributte is set it will return a window opbject for selected id.
-    *
-    * @return an array of AppWindow objects (https://developer.chrome.com/apps/app_window#type-AppWindow)
-    */
-   query: function(){
-     var result = [];
-     if(this.name){
-       var _win = chrome.app.window.get(this.name);
-       if(_win !== null){
-         result.push(_win);
-       }
-     } else {
-       result = chrome.app.window.getAll();
-     }
-     return result;
-   },
+  /**
+   * Query for opened by the app windows.
+   * If `name` atributte is set it will return a window opbject for selected id.
+   *
+   * @return an array of AppWindow objects
+   * (https://developer.chrome.com/apps/app_window#type-AppWindow)
+   */
+  query: function() {
+    var result = [];
+    if (this.name) {
+      var _win = chrome.app.window.get(this.name);
+      if (_win !== null) {
+        result.push(_win);
+      }
+    } else {
+      result = chrome.app.window.getAll();
+    }
+    return result;
+  },
 
-   /**
-    * Focus on current window.
-    */
-  focus: function(){
-    if(!this.window){
+  /**
+   * Focus on current window.
+   */
+  focus: function() {
+    if (!this.window) {
       this._reportMissingWindow();
       return;
     }
@@ -238,79 +229,79 @@ Polymer('chrome-window', {
   },
 
   get fullscreen() {
-    if(!this.window){
+    if (!this.window) {
       this._reportMissingWindow();
       return undefined;
     }
 
-     return this.window.isFullscreen();
+    return this.window.isFullscreen();
   },
 
-  set fullscreen(state){
-    if(!this.window){
+  set fullscreen(state) {
+    if (!this.window) {
       this._reportMissingWindow();
       return;
     }
 
-    if(state){
+    if (state) {
       this.window.fullscreen();
     } else {
 
       // it looks like code below doeasn't work. TODO: cancel fullscreen programatically.
 
       var defaultview = this.window.contentWindow.document.defaultView;
-      var keyboardEvent = document.createEvent("KeyboardEvent");
+      var keyboardEvent = document.createEvent('KeyboardEvent');
       Object.defineProperty(keyboardEvent, 'keyCode', {
-        get : function() {
+        get: function() {
           return this.keyCodeVal;
         }
       });
       Object.defineProperty(keyboardEvent, 'which', {
-        get : function() {
+        get: function() {
           return this.keyCodeVal;
         }
       });
-      keyboardEvent.initKeyboardEvent("keydown", true, true, defaultview, false, false, false, false, 27, 27);
+      keyboardEvent.initKeyboardEvent('keydown', true, true, defaultview,
+        false, false, false, false, 27, 27);
       keyboardEvent.keyCodeVal = 27;
       this.window.contentWindow.document.body.dispatchEvent(keyboardEvent);
-
     }
   },
 
-  maximize: function(){
-    if(!this.window){
+  maximize: function() {
+    if (!this.window) {
       this._reportMissingWindow();
       return;
     }
     this.window.maximize();
   },
 
-  minimize: function(){
-    if(!this.window){
+  minimize: function() {
+    if (!this.window) {
       this._reportMissingWindow();
       return;
     }
     this.window.minimize();
   },
 
-  restore: function(){
-    if(!this.window){
+  restore: function() {
+    if (!this.window) {
       this._reportMissingWindow();
       return;
     }
     this.window.restore();
   },
 
-  get alwaysOnTop(){
-    if(!this.window){
+  get alwaysOnTop() {
+    if (!this.window) {
       this._reportMissingWindow();
       return undefined;
     }
     return this.window.isAlwaysOnTop();
   },
 
-  set alwaysOnTop(state){
-    if(!this.window){
+  set alwaysOnTop(state) {
+    if (!this.window) {
       this._reportMissingWindow();
       return;
     }
@@ -318,12 +309,10 @@ Polymer('chrome-window', {
     this.window.setAlwaysOnTop(state);
   },
 
-
-
-
-  _reportMissingWindow: function(){
+  _reportMissingWindow: function() {
     console.error('There\'s no widnow object');
-    this.fire('error',{'message':'No active window available'});
+    this.fire('error', {
+      'message': 'No active window available'
+    });
   }
-
 });
